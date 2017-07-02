45b198dee1367ab14b54bc6582b9415ff1d960f7
Trevor Vincent <tvincent@cita.utoronto.ca>
2017-07-02 19:07:32 -0400

    Futher cleanup

--- Estimators/d4est_estimator_stats.c
+++ Estimators/d4est_estimator_stats.c
@@ -1,5 +1,5 @@
 #define _GNU_SOURCE
-#include <estimator_stats.h>
+#include <d4est_estimator_stats.h>
 #include <d4est_element_data.h>
 #include <util.h>
 #include <stdlib.h>
@@ -9,10 +9,10 @@
 #include <math.h>
 #include <sc_reduce.h>
 
-void estimator_stats_compute
+void d4est_estimator_stats_compute
 (
  p4est_t* p4est,
- estimator_stats_t* stats,
+ d4est_estimator_stats_t* stats,
  int curved
 )
 {
@@ -36,7 +36,7 @@ void estimator_stats_compute
 
   stats->tree = -1;
   stats->mpirank = p4est->mpirank;
-  estimator_stats_compute_stats
+  d4est_estimator_stats_compute_stats
     (
      stats,
      eta2,
@@ -48,10 +48,10 @@ void estimator_stats_compute
 }
 
 double
-estimator_stats_compute_per_bin
+d4est_estimator_stats_compute_per_bin
 (
  p4est_t* p4est,
- estimator_stats_t** stats,
+ d4est_estimator_stats_t** stats,
  int num_bins,
  int(*in_bin)(d4est_element_data_t*,int)
 )
@@ -83,7 +83,7 @@ estimator_stats_compute_per_bin
 
         stats[b]->tree = -1;
         stats[b]->mpirank = p4est->mpirank;
-        estimator_stats_compute_stats
+        d4est_estimator_stats_compute_stats
           (
            stats[b],
            eta2,
@@ -98,7 +98,7 @@ estimator_stats_compute_per_bin
   return local_eta2;
 }
 
-double estimator_stats_compute_per_tree(p4est_t* p4est, estimator_stats_t** stats, int curved){
+double d4est_estimator_stats_compute_per_tree(p4est_t* p4est, d4est_estimator_stats_t** stats, int curved){
 
   double* eta2 = P4EST_ALLOC_ZERO(double, p4est->local_num_quadrants);
   double local_eta2 = 0.;
@@ -124,7 +124,7 @@ double estimator_stats_compute_per_tree(p4est_t* p4est, estimator_stats_t** stat
 
       stats[tt]->tree = tt;
       stats[tt]->mpirank = p4est->mpirank;
-      estimator_stats_compute_stats
+      d4est_estimator_stats_compute_stats
         (
          stats[tt],
          eta2,
@@ -140,9 +140,9 @@ double estimator_stats_compute_per_tree(p4est_t* p4est, estimator_stats_t** stat
 
 
 void
-estimator_stats_compute_stats
+d4est_estimator_stats_compute_stats
 (
- estimator_stats_t* stats,
+ d4est_estimator_stats_t* stats,
  double* eta2,
  int sample_size,
  double total_eta2
@@ -190,9 +190,9 @@ estimator_stats_compute_stats
 }
 
 void
-estimator_stats_compute_max_percentiles_across_proc
+d4est_estimator_stats_compute_max_percentiles_across_proc
 (
- estimator_stats_t** stats,
+ d4est_estimator_stats_t** stats,
  int num_bins
 ){
   mpi_assert(num_bins < MAX_BINS);
@@ -230,9 +230,9 @@ estimator_stats_compute_max_percentiles_across_proc
 
 
 double
-estimator_stats_get_percentile
+d4est_estimator_stats_get_percentile
 (
- estimator_stats_t* stats,
+ d4est_estimator_stats_t* stats,
  int percentile
 ){
 
@@ -253,7 +253,7 @@ estimator_stats_get_percentile
 }
 
 
-void estimator_stats_print(estimator_stats_t* stats){
+void d4est_estimator_stats_print(d4est_estimator_stats_t* stats){
   printf("mpirank = %d\n", stats->mpirank);
   if(stats->tree == -1){
     printf("tree = ALL TREES\n");
@@ -266,141 +266,9 @@ void estimator_stats_print(estimator_stats_t* stats){
   printf("std = %.25f\n", stats->std);
   printf("max = %.25f\n", stats->max);
   printf("min = %.25f\n", stats->min);
-  /* printf("Q1 = %.25f\n", stats->Q1); */
-  /* printf("Q3 = %.25f\n", stats->Q3); */
   printf("p5 = %.25f\n", stats->p5);
   printf("p10 = %.25f\n", stats->p10);
   printf("p15 = %.25f\n", stats->p15);
   printf("p20 = %.25f\n", stats->p20);
-  /* printf("IQR = %.25f\n", stats->IQR); */
-  /* printf("num_bins_scott = %d\n", stats->num_bins_scott); */
-  /* printf("num_bins_freedman = %d\n", stats->num_bins_freedman); */
-  /* printf("bins_size_scot = %.25f\n", stats->bin_size_scott); */
-  /* printf("bins_size_freedman = %.25f\n", stats->bin_size_freedman); */
 }
 
-void estimator_stats_write_to_file
-(
- p4est_t* p4est,
- estimator_stats_t* stats,
- const char* file_name_stats,
- const char* file_name_estimator_prefix,
- int curved,
- int mpi_rank,
- int print_gnuplot
-)
-{
-  if (mpi_rank == 0){
-    FILE* f = fopen(file_name_stats, "w");
-    if (f == NULL){
-      mpi_abort("Error opening file");
-    }
-    fprintf(f,"sample_size = %d\n", stats->sample_size);   
-    fprintf(f,"mean = %.25f\n", stats->mean);
-    fprintf(f,"std = %.25f\n", stats->std);
-    fprintf(f,"max = %.25f\n", stats->max);
-    fprintf(f,"min = %.25f\n", stats->min);
-    fprintf(f,"p5 = %.25f\n", stats->p5);
-    fprintf(f,"p10 = %.25f\n", stats->p10);
-    fprintf(f,"p15 = %.25f\n", stats->p15);
-    fprintf(f,"p20 = %.25f\n", stats->p20);
-    /* fprintf(f,"Q1 = %.25f\n", stats->Q1); */
-    /* fprintf(f,"Q3 = %.25f\n", stats->Q3);  */
-    /* fprintf(f,"IQR = %.25f\n", stats->IQR); */
-    /* fprintf(f,"num_bins_scott = %d\n", stats->num_bins_scott); */
-    /* fprintf(f,"num_bins_freedman = %d\n", stats->num_bins_freedman); */
-    /* fprintf(f,"bins_size_scot = %.25f\n", stats->bin_size_scott); */
-    /* fprintf(f,"bins_size_freedman = %.25f\n", stats->bin_size_freedman); */
-    fclose(f);
-
-    
-    if(print_gnuplot){
-
-      char* gnuplot_freedman_save_as = NULL;
-      D4EST_ASPRINTF(gnuplot_freedman_save_as, "%s_%s","freedman_gnuplot", file_name_estimator_prefix );
-      FILE* file_freedman = fopen(gnuplot_freedman_save_as, "w");  
-      if (file_freedman == NULL){
-        mpi_abort("Error opening file");
-      }
-      free(gnuplot_freedman_save_as);
-      
-      fprintf(file_freedman,"reset \n");
-      /* fprintf(file_freedman,"n=%d #number of intervals\n",stats->num_bins_freedman); */
-      fprintf(file_freedman,"max=%.25f #max value\n",stats->max);
-      fprintf(file_freedman,"min=%.25f #min value\n",stats->min);
-      /* fprintf(file_freedman,"width=%.25f #interval width\n",stats->bin_size_freedman); */
-      fprintf(file_freedman,"#function used to map a value to the intervals\n");
-      fprintf(file_freedman,"hist(x,width)=width*floor(x/width)+width/2.0\n");
-      fprintf(file_freedman,"set term png #output terminal and file\n");
-      fprintf(file_freedman,"set output \"freedman_histogram.png\"\n");
-      fprintf(file_freedman,"set xrange [min:max]\n");
-      fprintf(file_freedman,"set yrange [0:]\n");
-      fprintf(file_freedman,"#to put an empty boundary around the\n");
-      fprintf(file_freedman,"#data inside an autoscaled graph.\n");
-      fprintf(file_freedman,"set offset graph 0.05,0.05,0.05,0.0\n");
-      fprintf(file_freedman,"set xtics min,(max-min)/5,max\n");
-      fprintf(file_freedman,"set boxwidth width*0.9\n");
-      fprintf(file_freedman,"set style fill solid 0.5 #fillstyle\n");
-      fprintf(file_freedman,"set tics out nomirror\n");
-      fprintf(file_freedman,"set xlabel \"eta2\"\n");
-      fprintf(file_freedman,"set ylabel \"Frequency\"\n");
-      fprintf(file_freedman,"#count and plot\n");
-      fprintf(file_freedman,"plot \"%s\" u (hist($1,width)):(1.0) smooth freq w boxes lc rgb \"green\" notitle\n",file_name_estimator_prefix);
-      fclose(file_freedman);
-  
-
-      char* gnuplot_scott_save_as = NULL;
-      D4EST_ASPRINTF(gnuplot_scott_save_as, "%s_%s", "scott_gnuplot", file_name_estimator_prefix);
-      FILE* file_scott = fopen(gnuplot_scott_save_as, "w");  
-      if (file_scott == NULL){
-        mpi_abort("Error opening file");
-      }
-      free(gnuplot_scott_save_as);
-      
-      fprintf(file_scott,"reset \n");
-      /* fprintf(file_scott,"n=%d #number of intervals\n",stats->num_bins_scott); */
-      fprintf(file_scott,"max=%.25f #max value\n",stats->max);
-      fprintf(file_scott,"min=%.25f #min value\n",stats->min);
-      /* fprintf(file_scott,"width=%.25f #interval width\n",stats->bin_size_scott); */
-      fprintf(file_scott,"#function used to map a value to the intervals\n");
-      fprintf(file_scott,"hist(x,width)=width*floor(x/width)+width/2.0\n");
-      fprintf(file_scott,"set term png #output terminal and file\n");
-      fprintf(file_scott,"set output \"scott_histogram.png\"\n");
-      fprintf(file_scott,"set xrange [min:max]\n");
-      fprintf(file_scott,"set yrange [0:]\n");
-      fprintf(file_scott,"#to put an empty boundary around the\n");
-      fprintf(file_scott,"#data inside an autoscaled graph.\n");
-      fprintf(file_scott,"set offset graph 0.05,0.05,0.05,0.0\n");
-      fprintf(file_scott,"set xtics min,(max-min)/5,max\n");
-      fprintf(file_scott,"set boxwidth width*0.9\n");
-      fprintf(file_scott,"set style fill solid 0.5 #fillstyle\n");
-      fprintf(file_scott,"set tics out nomirror\n");
-      fprintf(file_scott,"set xlabel \"eta2\"\n");
-      fprintf(file_scott,"set ylabel \"Frequency\"\n");
-      fprintf(file_scott,"#count and plot\n");
-      fprintf(file_scott,"plot \"%s\" u (hist($1,width)):(1.0) smooth freq w boxes lc rgb \"green\" notitle\n",file_name_estimator_prefix);
-      fclose(file_scott);
-    }
-  }
-  
-    
-  char* rank_save_as = NULL;
-  D4EST_ASPRINTF(rank_save_as, "%s_%d", file_name_estimator_prefix, mpi_rank);
-  FILE* file_est = fopen(rank_save_as, "w");
-  free(rank_save_as);
-
-  for (p4est_topidx_t tt = p4est->first_local_tree;
-       tt <= p4est->last_local_tree;
-       ++tt)
-    {
-      p4est_tree_t* tree = p4est_tree_array_index (p4est->trees, tt);
-      sc_array_t* tquadrants = &tree->quadrants;
-      int Q = (p4est_locidx_t) tquadrants->elem_count;
-      for (int q = 0; q < Q; ++q) {
-        p4est_quadrant_t* quad = p4est_quadrant_array_index (tquadrants, q);
-        fprintf(file_est, "%.20f\n", ((d4est_element_data_t*)(quad->p.user_data))->local_estimator);
-      }
-    }
-  
-  fclose(file_est);
-}
--- Estimators/d4est_estimator_stats.h
+++ Estimators/d4est_estimator_stats.h
@@ -1,5 +1,5 @@
-#ifndef ESTIMATOR_STATS_H
-#define ESTIMATOR_STATS_H 
+#ifndef D4EST_ESTIMATOR_STATS_H
+#define D4EST_ESTIMATOR_STATS_H 
 
 #include <pXest.h>
 #include <d4est_element_data.h>
@@ -15,48 +15,21 @@ typedef struct {
   double std;
   double max;
   double min;
-  /* double median; */
-  /* double Q1; */
-  /* double Q3; */
-  /* double IQR; */
-  /* double customQ; */
   double p5;
   double p10;
   double p15;
   double p20;
   double p25;
-  
-  /* bin size from scott's rule */
-  /* double bin_size_scott; */
-  /* double bin_size_freedman; */
-  /* int num_bins_scott; */
-  /* int num_bins_freedman; */
   int sample_size;
   
-} estimator_stats_t;
+} d4est_estimator_stats_t;
 
-/* This file was automatically generated.  Do not edit! */
-void estimator_stats_write_to_file(p4est_t *p4est,estimator_stats_t *stats,const char *file_name_stats,const char *file_name_estimator_prefix,int curved,int mpi_rank,int print_gnuplot);
-void estimator_stats_print(estimator_stats_t *stats);
-double estimator_stats_get_percentile(estimator_stats_t *stats,int percentile);
-double estimator_stats_compute_per_tree(p4est_t *p4est,estimator_stats_t **stats,int curved);
-void estimator_stats_compute_stats(estimator_stats_t *stats,double *eta2,int sample_size,double total_eta2);
-void estimator_stats_compute(p4est_t *p4est,estimator_stats_t *stats,int curved);
-void
-estimator_stats_compute_max_percentiles_across_proc
-(
- estimator_stats_t** stats,
- int num_trees
-);
-
-
-double
-estimator_stats_compute_per_bin
-(
- p4est_t* p4est,
- estimator_stats_t** stats,
- int num_bins,
- int(*in_bin)(d4est_element_data_t*,int)
-);
+void d4est_estimator_stats_print(d4est_estimator_stats_t *stats);
+double d4est_estimator_stats_get_percentile(d4est_estimator_stats_t *stats,int percentile);
+void d4est_estimator_stats_compute_max_percentiles_across_proc(d4est_estimator_stats_t **stats,int num_bins);
+double d4est_estimator_stats_compute_per_tree(p4est_t *p4est,d4est_estimator_stats_t **stats,int curved);
+double d4est_estimator_stats_compute_per_bin(p4est_t *p4est,d4est_estimator_stats_t **stats,int num_bins,int(*in_bin)(d4est_element_data_t *,int));
+void d4est_estimator_stats_compute_stats(d4est_estimator_stats_t *stats,double *eta2,int sample_size,double total_eta2);
+void d4est_estimator_stats_compute(p4est_t *p4est,d4est_estimator_stats_t *stats,int curved);
 
 #endif
