8a2f816ff837b8ffa0343d63b0a58eda47b08039
Trevor Vincent <tvincent@cita.utoronto.ca>
2017-07-26 00:47:11 -0400

    Remove old curved infra files

--- hpAMR/hp_amr_curved_uniform.h
+++
@@ -1,8 +0,0 @@
-#ifndef AMR_CURVED_UNIFORM_H
-#define AMR_CURVED_UNIFORM_H 
-
-amr_scheme_t *amr_curved_uniform_init();
-void amr_curved_uniform_destroy(amr_scheme_t *scheme);
-void amr_curved_uniform_set_refinement(p4est_iter_volume_info_t *info,void *user_data);
-
-#endif
