1adb3f0bbdb511d5f442f5f26120021f720edc4c
Trevor Vincent <tvincent@cita.utoronto.ca>
2018-03-08 01:10:49 -0500

    Make regression tests output more

--- Tests/Regression/d4est_test_cds.sh
+++ Tests/Regression/d4est_test_cds.sh
@@ -3,13 +3,25 @@ echo ${PWD}
 sed 's|num_of_amr_steps = .*|num_of_amr_steps = 1|g' ../ConstantDensityStar/options.input > options.input
 sed -i 's|ksp_type = .*|ksp_type = cg|g' options.input
 sed -i 's|ksp_do_not_use_preconditioner = 0|ksp_do_not_use_preconditioner = 1|g' options.input
-RES=$(../ConstantDensityStar/constant_density_star_driver options.input | grep -c 0.0000318356651187705076136)
+../ConstantDensityStar/constant_density_star_driver options.input 2&>1 disco4est.out
+RES=$(cat disco4est.out | grep -c 0.0000318356651187705076136)
 rm options.input
 if [ $RES -ne "1" ]; then
     echo "Test Fail"
+    echo "****************** CDS REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT BEGIN *****************"
+    cat disco4est.out
+    echo "****************** CDS REGRESSION TEST OUTPUT END *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT END *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT END *****************"
+    echo "****************** CDS REGRESSION TEST OUTPUT END *****************"
+    rm disco4est.out
     exit 1
 else
-    echo "Test Success"
+    echo "CDS Regression Test Success"
+    rm disco4est.out
 fi
 
 
--- Tests/Regression/d4est_test_stamm.sh
+++ Tests/Regression/d4est_test_stamm.sh
@@ -2,14 +2,25 @@ echo "Starting Stamm Regression test"
 echo ${PWD}
 sed 's|num_of_amr_steps = .*|num_of_amr_steps = 9|g' ../Stamm/options.input > stamm_options.input
 # RES=$(../Stamm/stamm_driver stamm_options.input | grep -c "0.0000001165842921684272017 0.0000007074379709713716485 0.0000146083448435542040227 0.0001316349774934072013998")
-RES=$(../Stamm/stamm_driver stamm_options.input | grep -c "0.0000000013301248793227330 0.0000000171172621537055147 0.0000013032058553497301960 0.0000007629001363926490040")
-
+../Stamm/stamm_driver stamm_options.input 2>&1 disco4est.out
+RES=$(cat disco4est.out | grep -c "0.0000000013301248793227330 0.0000000171172621537055147 0.0000013032058553497301960 0.0000007629001363926490040")
 rm stamm_options.input
 if [ $RES -ne "1" ]; then
     echo "Test Fail"
+    echo "****************** STAMM REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT BEGIN *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT BEGIN *****************"
+    cat disco4est.out
+    echo "****************** STAMM REGRESSION TEST OUTPUT END *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT END *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT END *****************"
+    echo "****************** STAMM REGRESSION TEST OUTPUT END *****************"
+    rm disco4est.out
     exit 1
 else
     echo "Test Success"
+    rm disco4est.out
 fi
 
 
