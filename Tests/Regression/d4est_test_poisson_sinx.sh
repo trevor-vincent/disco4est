7c48778447a1594bfae1ffadfeba4019868f96cc
Trevor Vincent <tvincent@cita.utoronto.ca>
2018-03-13 16:05:35 -0400

    Add Poisson Sinx regression test

--- Tests/Regression/d4est_test_poisson_sinx.sh
+++ Tests/Regression/d4est_test_poisson_sinx.sh
@@ -1,11 +1,9 @@
-echo "Starting ConstantDensityStar Regression test"
+echo "Starting PoisosnSinx Regression test"
 echo ${PWD}
-sed 's|num_of_amr_steps = .*|num_of_amr_steps = 1|g' ../ConstantDensityStar/options.input > cds_options.input
-sed -i 's|ksp_type = .*|ksp_type = cg|g' cds_options.input
-sed -i 's|ksp_do_not_use_preconditioner = 0|ksp_do_not_use_preconditioner = 1|g' cds_options.input
-../ConstantDensityStar/constant_density_star_driver cds_options.input > disco4est.out
-RES=$(cat disco4est.out | grep -c 0.0000318356651187705076136)
-rm cds_options.input
+cp ../PoissonSinx/options.input poisson_sinx_options.input
+../PoissonSinx/poisson_sinx_uniform_driver poisson_sinx_options.input > disco4est.out
+RES=$(cat disco4est.out | grep -c "027 FNRMSQR 0.000000000000000000000378767161")
+rm poisson_sinx_options.input
 if [ $RES -ne "1" ]; then
     echo "Test Fail"
     echo "****************** CDS REGRESSION TEST OUTPUT BEGIN *****************"
@@ -20,7 +18,7 @@ if [ $RES -ne "1" ]; then
     rm disco4est.out
     exit 1
 else
-    echo "CDS Regression Test Success"
+    echo "Poisson Sinx Regression Test Success"
     rm disco4est.out
 fi
 
