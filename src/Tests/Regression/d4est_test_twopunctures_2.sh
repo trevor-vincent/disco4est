90746cf1c88358bc5534b340094108f576a87495
Trevor Vincent <tvincent@cita.utoronto.ca>
2018-08-28 16:14:05 -0400

    update two punctures test

--- src/Tests/Regression/d4est_test_twopunctures_2.sh
+++ src/Tests/Regression/d4est_test_twopunctures_2.sh
@@ -2,7 +2,7 @@ echo "Starting Two Punctures Regression test"
 echo ${PWD}
 sed 's|num_of_amr_steps = .*|num_of_amr_steps = 0|g' ../TwoPunctures/options_13tree.input > two_punctures_options.input
 ../TwoPunctures/two_punctures_robin_diff_geom_estimator_global_13tree_driver two_punctures_options.input > disco4est.out
-RES=$(cat disco4est.out | grep -c "104 832 832 10338.036508042")
+RES=$(cat disco4est.out | grep -c "832.0000000000000000 0.000169047")
 rm two_punctures_options.input
 if [ $RES -ne "1" ]; then
     echo "Test Fail"
