61aa3abaa1869647e0c6ef5c46c65e1109391b58
Trevor Vincent <tvincent@cita.utoronto.ca>
2018-12-24 21:26:01 -0500

    Better blas cmake file

--- cmake/openblas.cmake
+++ cmake/openblas.cmake
@@ -3,52 +3,55 @@ include (ExternalProject)
 #
 # Bundled openblas paths.
 #
-set(OPENBLAS_BUNDLED_PREFIX "${PROJECT_BINARY_DIR}/third_party/OpenBLAS/install")
-set(OPENBLAS_BUNDLED_LIB    "${OPENBLAS_BUNDLED_PREFIX}/lib/libopenblas.a")
+set(BLAS_BUNDLED_PREFIX "${PROJECT_BINARY_DIR}/third_party/OpenBLAS/install")
+set(BLAS_BUNDLED_LIB    "${BLAS_BUNDLED_PREFIX}/lib/libopenblas.a")
 
 macro(openblas_use_bundled)
-  set(OPENBLAS_PREFIX "${OPENBLAS_BUNDLED_PREFIX}")
-  set(OPENBLAS_INCLUDE_DIRS "${OPENBLAS_BUNDLED_PREFIX}/include")
-  set(OPENBLAS_LIBRARIES "${OPENBLAS_BUNDLED_LIB}")
-  set(ENABLE_BUNDLED_OPENBLAS True)
+  set(BLAS_PREFIX "${BLAS_BUNDLED_PREFIX}")
+  set(BLAS_INCLUDE_DIRS "${BLAS_BUNDLED_PREFIX}/include")
+  set(BLAS_LIBRARIES "${BLAS_BUNDLED_LIB}")
+  set(ENABLE_BUNDLED_BLAS True)
+  openblas_build()
 endmacro()
 
 macro(openblas_try_system)
+  message(STATUS "******Trying to find blas on system******.")
   find_package(BLAS REQUIRED)
   message(STATUS "Found a system-wide openblas.")
 endmacro()
 
 macro(openblas_try_prefix)
-  find_path(OPENBLAS_INCLUDE_DIRS lapacke.h ${OPENBLAS_PREFIX}/include NO_DEFAULT_PATH)
-  find_library(OPENBLAS_LIB libopenblas.a ${OPENBLAS_PREFIX}/lib NO_DEFAULT_PATH)
+  message(STATUS "*******Trying to use blas prefix*******.")
+  find_path(BLAS_INCLUDE_DIRS lapacke.h ${BLAS_PREFIX}/include NO_DEFAULT_PATH)
+  find_library(BLAS_LIB libopenblas.a ${BLAS_PREFIX}/lib NO_DEFAULT_PATH)
 
-  if(OPENBLAS_INCLUDE_DIRS AND OPENBLAS_LIB)
-    set(OPENBLAS_LIBRARIES ${OPENBLAS_LIB})
+  if(BLAS_INCLUDE_DIRS AND BLAS_LIB)
+    set(BLAS_LIBRARIES ${BLAS_LIB})
   else()
-    message(FATAL_ERROR "Couldn't find openblas in '${OPENBLAS_PREFIX}'")
+    message(FATAL_ERROR "Couldn't find openblas in '${BLAS_PREFIX}'")
   endif()
 endmacro()
 
-if (OPENBLAS_PREFIX)
+if (BLAS_PREFIX)
   openblas_try_prefix()
-elseif (NOT ENABLE_BUNDLED_OPENBLAS)
+elseif (NOT ENABLE_BUNDLED_BLAS)
   openblas_try_system()
 else()
   openblas_use_bundled()
 endif()
 
-include_directories(${OPENBLAS_INCLUDE_DIRS})
+include_directories(${BLAS_INCLUDE_DIRS})
 
-message(STATUS "Use openblas includes: ${OPENBLAS_INCLUDE_DIRS}")
-message(STATUS "Use openblas library: ${OPENBLAS_LIBRARIES}")
+message(STATUS "Use openblas includes: ${BLAS_INCLUDE_DIRS}")
+message(STATUS "Use openblas library: ${BLAS_LIBRARIES}")
 
 macro(openblas_build)
   ExternalProject_Add(openblas
     PREFIX              ${CMAKE_BINARY_DIR}/third_party/OpenBLAS
     SOURCE_DIR          ${CMAKE_SOURCE_DIR}/third_party/OpenBLAS
     CONFIGURE_COMMAND   ""
-    BUILD_COMMAND       cd ${CMAKE_SOURCE_DIR}/third_party/OpenBLAS && make PREFIX=${OPENBLAS_BUNDLED_PREFIX}
-    INSTALL_COMMAND     cd ${CMAKE_SOURCE_DIR}/third_party/OpenBLAS && make PREFIX=${OPENBLAS_BUNDLED_PREFIX} install
+    BUILD_COMMAND       cd ${CMAKE_SOURCE_DIR}/third_party/OpenBLAS && make PREFIX=${BLAS_BUNDLED_PREFIX}
+    INSTALL_COMMAND     cd ${CMAKE_SOURCE_DIR}/third_party/OpenBLAS && make PREFIX=${BLAS_BUNDLED_PREFIX} install
     CMAKE_ARGS
     -DCMAKE_CXX_FLAGS:STRING=-Wunused-but-set-variable -Wunused-variable -Wincompatible-pointer-types -Wformat-nonliteral -Wstrict-overflow -Wconversion -Wunused-but-set-variable -Wunsafe-loop-optimizations -Wunused-parameter -Wlarger-than= -Wdiscarded-qualifiers -Wfloat-equal -Wmaybe-uninitialized -Wcomment
     -DCMAKE_C_FLAGS:STRING=-Wunused-but-set-variable -Wunused-variable -Wincompatible-pointer-types -Wformat-nonliteral -Wstrict-overflow -Wconversion -Wunused-but-set-variable -Wunsafe-loop-optimizations -Wunused-parameter -Wlarger-than= -Wdiscarded-qualifiers -Wfloat-equal -Wmaybe-uninitialized -Wcomment
@@ -57,6 +60,6 @@ macro(openblas_build)
 endmacro()
 
 
-if(ENABLE_BUNDLED_OPENBLAS)
-  openblas_build()
-endif()
+# if(ENABLE_BUNDLED_BLAS)
+
+# endif()
